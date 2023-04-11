1b4b83a2295dc5e3dd5bbd8823648cd1d6f83fec
Samuel Silva <samuelmec1@gmail.com>
2023-04-11 14:21:47 -0300

    Formularios SignIn e SignOut

--- reactPapo/src/components/FormSignIn/styles.ts
+++ reactPapo/src/components/FormSignIn/styles.ts
@@ -1,28 +1,45 @@
 import styled from "styled-components";
 
 export const ContainerForm = styled.form`
-  width: 100%;
-  height: 250px;
+  width: 400px;
+  height: 350px;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
   gap: 10px;
+
+  p{
+    color: #ffff;
+    font-size: 20px;
+  }
+  
+  button{
+    width: 230px;
+    height: 60px;
+    border: 0;
+    background: #1644FF;
+    box-shadow: 0px 13.75px 55px -16.5px #1351B4;
+    border-radius: 11px;
+    margin: 0 auto;
+
+    font-size: 18px;
+    color: #FFFF;
+  }
 `
 
 export const Input = styled.input`
-  width: 300px;
-  height: 50px;
+  width: 400px;
+  height: 60px;
   padding: 5px;
-  background-color: #AAACAF;
-  color: rgb(150,150,150);
   border: 0;
   font-size: 16px;
-  border-radius: 5px;
-`
+  margin: 0 auto;
+  margin-bottom: 10px;
+  
+  background: #FFFFFF;
+  box-shadow: 0px 13.75px 55px rgba(0, 0, 0, 0.337);
+  border-radius: 13.75px;
 
-export const Label = styled.p`
-  color: #ffff;
-  display: block;
-  text-align: start;
+  :focus{
+    outline: 0;
+  }
 `
--- reactPapo/src/components/FormSignOut/index.tsx
+++ reactPapo/src/components/FormSignOut/index.tsx
@@ -1,5 +1,5 @@
-import { ContainerForm, Input } from "./styles";
-import LoginUserFormSchema from '../../validations/validationLogin'
+import { ContainerForm, Input } from "../FormSignIn/styles";
+import SignOutFormSchema from '../../validations/validationLogin'
 import {yupResolver} from "@hookform/resolvers/yup"
 import {SubmitHandler, useForm} from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
@@ -7,11 +7,12 @@ import { useState } from "react";
 // import useAuth from "../../hooks/HookAuthContext";
 
 type LoginUserData = {
+  usuario: string;
   email: string;
   password: string;
 }
 
-export function FormularioLogin(){
+export function FormularioSignOut(){
   // const {signin} = useAuth()
   const [error, setError] = useState<string | boolean>()
   const navigate = useNavigate()
@@ -20,7 +21,7 @@ export function FormularioLogin(){
     handleSubmit, 
     formState: {errors}, 
     reset
-  } = useForm<LoginUserData>({resolver: yupResolver(LoginUserFormSchema)})
+  } = useForm<LoginUserData>({resolver: yupResolver(SignOutFormSchema)})
 
   const handlerCreatedUser: SubmitHandler<LoginUserData> = async (values) => {  
       console.log(values)
@@ -30,14 +31,18 @@ export function FormularioLogin(){
     <ContainerForm action='' onSubmit={handleSubmit(handlerCreatedUser)}>
 
       <p>Usuario</p>
-      <Input type="text" placeholder='Usuario' {...register("email")}/>
+      <Input type="text" placeholder='Usuario' {...register("usuario")}/>
+      {errors.email?.message && <p style={{color: 'red'}}>{errors.email.message}</p>}
+
+      <p>E-mail</p>
+      <Input type="text" placeholder='Email' {...register("email")}/>
       {errors.email?.message && <p style={{color: 'red'}}>{errors.email.message}</p>}
 
       <p>Senha</p>
       <Input type="password" placeholder='Senha' {...register("password")}/>
       {errors.password?.message && <p style={{color: 'red'}}>{errors.password.message}</p>}
 
-      <button type="submit" title='Entrar'>Entrar</button>
+      <button type="submit" title='Entrar'>Cadastrar</button>
       {error && <p style={{color: 'red'}}>{error}</p>}
     </ContainerForm>
   )
