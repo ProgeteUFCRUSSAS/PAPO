eb146578bc231e09096f4551fd65e3e9c3dc9dc2
Samuel Silva <samuelmec1@gmail.com>
2023-04-28 09:51:46 -0300

    Adicionando novos componentes nas rotas

--- reactPapo/src/Router.tsx
+++ reactPapo/src/Router.tsx
@@ -1,13 +1,26 @@
 import { Fragment } from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import { SignIn } from './pages/SignIn'
+import { Layout } from './components/Layout'
+import { SignOutPsicologos } from './pages/SignOut/Psicologo'
+import { SignOutUsuarios } from './pages/SignOut/Usuario'
 
 export function Router(){
   return(
     <BrowserRouter>
       <Fragment>
         <Routes>
-          <Route path='/' element={<SignIn />}/>
+          <Route path='/' element={<Layout />}>
+            {/* Quando a pagina inicio for criada deve 
+            importar ela aqui e colocar ela no lugar de SignIn no path="/", 
+            e depois desmarcar o que foi o que foi comentado apos essa linha. 
+            E a cada pagina que for adicionada dever criada apenas seu caminho aqui, 
+            pois o Header ira acompanhar em todas*/}
+            <Route path='/' element={<SignIn />}/>
+            {/* <Route path='/signIn' element={<SignIn />} /> */}
+            <Route path='/signOutUsuarios' element={<SignOutPsicologos />}/>
+            <Route path='/signOutPsicologos' element={<SignOutUsuarios />}/>
+          </Route>
         </Routes>
       </Fragment>
     </BrowserRouter>
