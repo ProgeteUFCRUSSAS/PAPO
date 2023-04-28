9527784e8eecb12c8249d1b862b526baf65e7b17
GabrielRRG <gabrielramalhorg@gmail.com>
2023-04-28 12:14:06 -0300

    Branch exclusiva minha por motivos de segurança

---
+++ reactPapo/src/components/HomeHero/Hero.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {HeroSection, HeroContainer, HeroHeader, HeroText, ButtonWrapper, HeroButton, HeroWrapper } from './HeroStyles';
+
+const Hero = () => {
+	return (
+		<HeroSection>
+			<HeroWrapper>
+			<HeroContainer>
+				<HeroHeader>Plataforma de atendimento psicológico online</HeroHeader>
+				<HeroText>
+					Essa plataforma tem como objetivo aumentar a velocidade e agendamento das sessões com os psicologos
+				</HeroText>
+				<ButtonWrapper>
+					<Link to="/">
+					Consultar horários
+					</Link>
+					{/* <HeroButton></HeroButton> */}
+				</ButtonWrapper>
+			</HeroContainer>
+			</HeroWrapper>
+		</HeroSection>
+	);
+};
+
+export default Hero;
