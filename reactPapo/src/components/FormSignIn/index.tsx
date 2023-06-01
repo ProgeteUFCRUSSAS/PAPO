071ca2237b630fa3272270341d4718b81d6710a5
Samuel Silva <samuelmec1@gmail.com>
2023-06-01 12:52:56 -0300

    contextAPI

--- reactPapo/src/components/FormSignIn/index.tsx
+++ reactPapo/src/components/FormSignIn/index.tsx
@@ -1,44 +1,52 @@
-import { ContainerForm, Input } from "./styles";
+import { ContainerForm, Input } from './styles'
 import LoginUserFormSchema from '../../validations/validationLogin'
-import {yupResolver} from "@hookform/resolvers/yup"
-import {SubmitHandler, useForm} from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
-import { useState } from "react";
-// import useAuth from "../../hooks/HookAuthContext";
+import { useContext, useState } from 'react'
+import { AuthContext } from '../../contexts/authUser'
 
 type LoginUserData = {
-  email: string;
-  password: string;
+  email: string
+  password: string
 }
 
-export function FormularioLogin(){
-  // const {signin} = useAuth()
+export function FormularioLogin() {
+  const { signIn, loadingAuth } = useContext(AuthContext)
+
   const [error, setError] = useState<string | boolean>()
   const navigate = useNavigate()
   const {
-    register, 
-    handleSubmit, 
-    formState: {errors}, 
+    register,
+    handleSubmit,
+    formState: { errors },
     reset
-  } = useForm<LoginUserData>({resolver: yupResolver(LoginUserFormSchema)})
+  } = useForm<LoginUserData>({ resolver: yupResolver(LoginUserFormSchema) })
 
-  const handlerCreatedUser: SubmitHandler<LoginUserData> = async (values) => {  
-      console.log(values)
+  const handlerCreatedUser: SubmitHandler<LoginUserData> = async values => {
+    await signIn(values.email, values.password)
+      .then(() => navigate('/'))
+      .catch(() => setError('Email ou senha invalida'))
   }
 
-  return(
-    <ContainerForm action='' onSubmit={handleSubmit(handlerCreatedUser)}>
-
+  return (
+    <ContainerForm action="" onSubmit={handleSubmit(handlerCreatedUser)}>
       <p>Usuario</p>
-      <Input type="text" placeholder='Usuario' {...register("email")}/>
-      {errors.email?.message && <p style={{color: 'red'}}>{errors.email.message}</p>}
+      <Input type="text" placeholder="Usuario" {...register('email')} />
+      {errors.email?.message && (
+        <p style={{ color: 'red' }}>{errors.email.message}</p>
+      )}
 
       <p>Senha</p>
-      <Input type="password" placeholder='Senha' {...register("password")}/>
-      {errors.password?.message && <p style={{color: 'red'}}>{errors.password.message}</p>}
+      <Input type="password" placeholder="Senha" {...register('password')} />
+      {errors.password?.message && (
+        <p style={{ color: 'red' }}>{errors.password.message}</p>
+      )}
 
-      <button type="submit" title='Entrar'>Entrar</button>
-      {error && <p style={{color: 'red'}}>{error}</p>}
+      <button type="submit" title="Entrar">
+        Entrar
+      </button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </ContainerForm>
   )
 }
--- reactPapo/src/components/FormSignOut/index.tsx
+++ reactPapo/src/components/FormSignOut/index.tsx
@@ -1,49 +1,73 @@
-import { ContainerForm, Input } from "../FormSignIn/styles";
+import { ContainerForm, Input } from '../FormSignIn/styles'
 import SignOutFormSchema from '../../validations/validationSignOut'
-import {yupResolver} from "@hookform/resolvers/yup"
-import {SubmitHandler, useForm} from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
-import { useState } from "react";
+import { useContext, useState } from 'react'
+import { AuthContext } from '../../contexts/authUser'
 // import useAuth from "../../hooks/HookAuthContext";
 
 type LoginUserData = {
-  usuario: string;
-  email: string;
-  password: string;
+  usuario: string
+  email: string
+  password: string
 }
 
-export function FormularioSignOut(){
-  // const {signin} = useAuth()
+export function FormularioSignOut() {
   const [error, setError] = useState<string | boolean>()
-  const navigate = useNavigate()
-  const {
-    register, 
-    handleSubmit, 
-    formState: {errors}, 
-    reset
-  } = useForm<LoginUserData>({resolver: yupResolver(SignOutFormSchema)})
 
-  const handlerCreatedUser: SubmitHandler<LoginUserData> = async (values) => {  
-      console.log(values)
+  const { signUp, loadingAuth } = useContext(AuthContext)
+
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm<LoginUserData>({ resolver: yupResolver(SignOutFormSchema) })
+
+  const handlerCreatedUser: SubmitHandler<LoginUserData> = async values => {
+    // console.log(values)
+    await signUp(values.usuario, values.email, values.password)
+      .then(() => {
+        console.log('Cadastro com sucesso')
+      })
+      .catch((error: any) => {
+        console.log(error)
+      })
+
+    reset()
   }
 
-  return(
-    <ContainerForm action='' onSubmit={handleSubmit(handlerCreatedUser)}>
-
+  return (
+    <ContainerForm action="" onSubmit={handleSubmit(handlerCreatedUser)}>
       <p>Usuario</p>
-      <Input type="text" placeholder='Usuario' {...register("usuario")}/>
-      {errors.usuario?.message && <p style={{color: 'red', fontSize: "14px"}}>{errors.usuario.message}</p>}
+      <Input type="text" placeholder="Usuario" {...register('usuario')} />
+      {errors.usuario?.message && (
+        <p style={{ color: 'red', fontSize: '14px' }}>
+          {errors.usuario.message}
+        </p>
+      )}
 
       <p>E-mail</p>
-      <Input type="text" placeholder='Email' {...register("email")}/>
-      {errors.email?.message && <p style={{color: 'red', fontSize: "14px"}}>{errors.email.message}</p>}
+      <Input type="text" placeholder="Email" {...register('email')} />
+      {errors.email?.message && (
+        <p style={{ color: 'red', fontSize: '14px' }}>{errors.email.message}</p>
+      )}
 
       <p>Senha</p>
-      <Input type="password" placeholder='Senha' {...register("password")}/>
-      {errors.password?.message && <p style={{color: 'red', fontSize: "14px"}}>{errors.password.message}</p>}
+      <Input type="password" placeholder="Senha" {...register('password')} />
+      {errors.password?.message && (
+        <p style={{ color: 'red', fontSize: '14px' }}>
+          {errors.password.message}
+        </p>
+      )}
 
-      <button type="submit" title='Entrar'>Cadastrar</button>
-      {error && <p style={{color: 'red', fontSize: "14px"}}>{error}</p>}
+      <button type="submit" title="Entrar">
+        Cadastrar
+      </button>
+      {error && <p style={{ color: 'red', fontSize: '14px' }}>{error}</p>}
     </ContainerForm>
   )
 }
