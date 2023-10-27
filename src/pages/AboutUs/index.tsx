9c0736c4d6f2a308e5be1735fe8b7c997498a3ca
Guilherme Venâncio <guilhermevenanciodasilva851@gmail.com>
2023-10-27 17:14:07 -0300

    Update styling for AboutUs page (generated by useblackbox.ai )

--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -9,18 +9,24 @@ export function AboutUs() {
   return (
     <div>
       <InicialPage>
-        <div class ="textoinicial">
-        <h1>Plataforma de atendimento <br />psicológico online</h1>
-        <p>Essa plataforma tem como objetivo <br />aumentar a velocidade e agendamento <br />das seções com os psicólogos.</p>
+        <div className ="textoinicial">
+        <h1>Plataforma de <br /> atendimento <br />psicológico online</h1>
+        <p>Essa plataforma tem como objetivo aumentar a velocidade e agendamento <br />das seções com os psicólogos.</p>
         </div>
-        <img src="src\assets\logoaboutus.svg" class ="logo" alt="logo PAPO" />
-        <img src="src\assets\waves.svg" class ="waveb" alt="wave azul" />    
-        <img src="src\assets\wave.svg" class ="wave" alt="wave" />
+        <div className='logo'><img src="src\assets\logoaboutus.svg" id='logo' alt="logo" /></div>
+        <div className='waves'>
+        <div className='wave'>
+          <img src="src\assets\wave.svg" id='wavea' alt="wave1" />
+        </div>
+        <div className='waveb'>
+          <img src="src\assets\waves.svg" id='waveb' alt="wave2" />
+        </div>
+        </div>
       </InicialPage>
       <OurVision>
-        <div class ="text">
+        <div className ="OV">
           <h1>Importancia  dos cuidados com a saúde mental <br /><br /></h1>
-          <h3 class ="OV">Nossa Visão:</h3><br />
+          <h3 >Nossa Visão:</h3><br />
               <p>Acreditamos que a saúde mental é a base do bem-estar e da qualidade<br />de vida. Na era contemporânea, enfrentamos desafios únicos que <br />
               podem afetar nossa paz interior e equilíbrio emocional. Portanto, a <br />"Plataforma de Desenvolvimento Profissional para Psicólogos" não <br /> 
               apenas se dedica ao aprimoramento dos profissionais, mas também a <br />promover uma mentalidade saudável e cuidados com a saúde mental <br />
--- src/pages/AboutUs/styles.ts
+++ src/pages/AboutUs/styles.ts
@@ -9,137 +9,158 @@ export const InicialPage = styled.div`
 
 
 .textoinicial{
-    width: 700px;
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    font-family: 'Ubuntu', sans-serif;
-
-}
-
-.textoinical, h1{
-    color: #38B6FF;
-    font-size: 50px;
-    margin-top: 5rem;
     margin-left: 100px;
-    
-}
-p{
-    color: #38B6FF;
-    font-size: 30px;
-    margin-top: 2rem;
-    margin-bottom: 6.25rem;
-    margin-left: 6.25rem;
-
+    margin-top: 120px;
+    font-family: 'Ubuntu', sans-serif;
 }
 
-.logo{
-    margin-top: 5rem; 
-    margin-bottom: 50px;
+#logo{
     margin-left: 1000px;
+    margin-top: -250px;
 }
 
+.textoinicial p{
+    font-size: 24px;
+    margin-left: 60px;
+    align-content: right; 
+}
+
+.textoinicial h1{
+    font-size: 42px; 
+}
+
+.waves{
+    margin-bottom: 600px;
+    align-items: center;
+}
 .wave{
-    margin-left: 0;
-    flex-direction: column;
-    width: 100%; 
-    position; relative;
-    z-index: 2;
-    left: 0;
-    rigth: 0;
-} 
+    position: absolute;
+    margin-bottom: 510px;
+}
 
 .waveb{
-    flex-direction: column;
-    width: 100%;
     position: absolute;
-    height: 500px;
-    margin-top: 325px;
-    left: 0;
-    rigth: 0;
-    z-index: 1;
+
 }
-@media (max-width: 1199px) {
-    .textoinicial, h1{
+
+#wavea{
+    height: 575px;
+    width: 100%;
+} 
+
+#waveb{
+    height: 450px;
+    width: 100%;
+}
+
+@media (max-width: 1200px) {
+    .textoinicial{
         font-size: 45px;
-        margin-left: 90px
+        margin-left: 80px
         margin-top: 50px;
     }
 
-    p{
-        color: #38B6FF;
-        font-size: 25px;
-        margin-top: 2rem;
-        margin-bottom: 6.25rem;
-        margin-left: 6.25rem;
-    
-    }
-    
-    .logo{
-        margin-top: 100px; 
-        margin-bottom: 1px;
-        margin-left: 700px;
+    .logo #logo{
+        width: 300px;
+        margin-top: -350px;
+        margin-left: 600px;
     }
 
-    .wave{
-        flex-direction: column;
-        width: 67rem;
-        left:0;
-        rigth: 0;
-        z-index: 2;
+    .textoinicial h1{
+        font-size: 35px;
+        font-weight: 600;
+        margin-left: 10px;
         margin-top: 20%;
-    
     }
 
+    .textoinicial p{
+        color: #38B6FF;
+        font-size: 20px;
+        margin-bottom: 6.25rem;
+        margin-left: 30px;
+    }
+
+    .waves{
+        margin-bottom: 600px;
+        align-items: center;
+    }
+    .wave{
+        position: absolute;
+        margin-bottom: 510px;
+    }
+    
     .waveb{
-        flex-direction: column;
-        width: 67rem;
-        margin-top: 525px;
-        z-index: 1;
-       
-        
+        position: absolute;
+    
+    }
+    
+    #wavea{
+        height: 595px;
+        width: 100%;
+    } 
+    
+    #waveb{
+        height: 550px;
+        width: 100%;
     }
 
     }
   
   @media (max-width: 919px) {
-    .textoinicial, h1{
-        font-size: 35px;
-        margin-left: 2rem;
+    .textoinicial{
+        margin-left: 3rem;
         margin-top: 50px;
     }
 
-    p{
+    .textoinicial h1{
         color: #38B6FF;
-        font-size: 22px;
-        margin-top: 2rem;
+        font-size: 40px;
+        margin-top: 100px;
         margin-bottom: 6.25rem;
-        margin-left: 2rem;
+        margin-left: 75px;
+    }
+
+    .textoinicial p{
+        color: #38B6FF;
+        font-size: 30px;
+        margin-top: -80px;
+        margin-right: 300px;
+        margin-left: 100px; 
+        
     
     }
     
-    .logo{
-        margin-top: 100px; 
+    .logo #logo{
+        margin-top: -50px; 
         margin-bottom: 1px;
-        margin-left: 500px;
-        width: 300px;
+        margin-left: 125px;
+        width: 250px;
     }
 
+    .waves{
+        margin-bottom: 600px;
+        align-items: center;
+    }
     .wave{
-        flex-direction: row;
-        width: 52.5rem;
-        margin-top: 200px;
+        position: absolute;
+        margin-bottom: 510px;
+    }
+    
+    .waveb{
+        position: absolute;
     
     }
-
-    .waveb{
-        flex-direction: column;
-        width: 53rem;
-        left:0;
-        rigth: 0;
-        margin-top: 3px; 
-        z-index: 1;
+    
+    #wavea{
+        height: 550px;
+        width: 100%;
+    } 
+    
+    #waveb{
+        height: 500px;
+        width: 100%;
     }
+
   }
 
   @media (max-width: 770px) {
@@ -156,27 +177,35 @@ p{
     
     }
     
-    .logo{
+    #logo{
         margin-top: 125px; 
         margin-bottom: 1px;
         margin-left: 150px;
         width: 200px;
     }
 
+    .waves{
+        margin-bottom: 600px;
+        align-items: center;
+    }
     .wave{
-        flex-direction: row;
-        width: 38.5rem;
-        margin-top: 150px;
+        position: absolute;
+        margin-bottom: 510px;
+    }
+    
+    .waveb{
+        position: absolute;
     
     }
-
-    .waveb{
-        flex-direction: column;
-        width: 39rem;
-        left:0;
-        rigth: 0;
-        margin-top: 38%;
-        z-index: 1;
+    
+    #wavea{
+        height: 550px;
+        width: 100%;
+    } 
+    
+    #waveb{
+        height: 500px;
+        width: 100%;
     }
   }
   
@@ -191,36 +220,46 @@ p{
 
     h1{
         font-size: 27px;
-        margin-top: 10px;
+        margin-top: -25px;
     }
 
-    p{
+    .textoinicial p{
         color: #38B6FF;
-        font-size: 20px;
-        margin-top: 18rem;
+        font-size: 24px;
+        margin-top: -70px;
         margin-bottom: 6.25rem;
-        margin-left: 2rem;
-    
+        margin-right: 100px;   
     }
     
-    .logo{
+    #logo{
         margin-top: 150px; 
         margin-bottom: 175px;
         margin-left: 100px;
         width: 250px;
     }
 
+    .waves{
+        margin-bottom: 600px;
+        align-items: center;
+    }
     .wave{
-        flex-direction: row;
-        width: 30rem;
-        margin-top: 1px;
+        position: absolute;
+        margin-bottom: 510px;
+    }
+    
+    .waveb{
+        position: absolute;
     
     }
-
-    .waveb{
-        flex-direction: row;
-        width: 30rem;
-        margin-top: 23rem;
+    
+    #wavea{
+        height: 550px;
+        width: 100%;
+    } 
+    
+    #waveb{
+        height: 515px;
+        width: 100%;
     }
     
   }
@@ -231,25 +270,31 @@ export const OurVision = styled.div`
 .text{
     margin-top: 100px;
     color: #38B6FF;
-    font-family: 'Ubuntu', bold;
-    font-weight: 700;
+    font-family: 'Ubuntu', sans-serif;
+    font-weight: 600;
 }
 
 h1{
-    font-size: 45px;
-    margin-left: 100px;
+    margin-left: 105px;
+    margin-top: 75px;
+    font-size: 42px;
+}
+
+h3{
+
+    margin-left: 150px;
+    font-size: 28px;
 }
 
 .OV{
-    font-family: 'Ubuntu', bold;
-    font-size: 30px;
-    margin-left: 125px;
+    font-family: 'Ubuntu', sans-serif;
+    color: #38B6FF;
 }
 
 p{
-    font-family: 'Ubuntu', regular;
-    font-size: 30px;
-    margin-left: 125px;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 28px;
+    margin-left: 150px;
     margin-bottom: 100px;
     font-weight: 300;
 }
@@ -257,7 +302,7 @@ p{
     .text{
         margin-top: 100px;
         color: #38B6FF;
-        font-family: 'Ubuntu', bold;
+        font-family: 'Ubuntu', sans-serif;
         font-weight: 700;
     }
     
@@ -267,15 +312,15 @@ p{
     }
     
     .OV{
-        font-family: 'Ubuntu', bold;
+        font-family: 'Ubuntu', sans-serif;
         font-size: 27px;
-        margin-left: 100px;
+        margin-left: 20px;
     }
     
     p{
-        font-family: 'Ubuntu', regular;
+        font-family: 'Ubuntu', sans-serif;
         font-size: 27px;
-        margin-left: 100px;
+        margin-left: 150px;
         margin-bottom: 100px;
         font-weight: 300;
     }
@@ -286,25 +331,29 @@ p{
     .text{
         margin-top: 100px;
         color: #38B6FF;
-        font-family: 'Ubuntu', bold;
+        font-family: 'Ubuntu', sans-serif;
         font-weight: 700;
     }
     
     h1{
         font-size: 35px;
-        margin-left: 50px;
+        margin-left: -20px;
     }
     
     .OV{
-        font-family: 'Ubuntu', bold;
+        font-family: 'Ubuntu', sans-serif;
         font-size: 27px;
         margin-left: 75px;
     }
     
+    h3{
+        margin-left: 50px;
+    }
+
     p{
-        font-family: 'Ubuntu', regular;
-        font-size: 27px;
-        margin-left: 75px;
+        font-family: 'Ubuntu', sans-serif;
+        font-size: 23px;
+        margin-left: 50px;
         margin-bottom: 100px;
         font-weight: 300;
     }
@@ -314,7 +363,7 @@ p{
     text{
         margin-top: 100px;
         color: #38B6FF;
-        font-family: 'Ubuntu', bold;
+        font-family: 'Ubuntu', sans-serif;
         font-weight: 700;
     }
     
@@ -324,13 +373,13 @@ p{
     }
     
     .OV{
-        font-family: 'Ubuntu', bold;
+        font-family: 'Ubuntu', sans-serif;
         font-size: 24px;
         margin-left: 20px;
     }
     
     p{
-        font-family: 'Ubuntu', regular;
+        font-family: 'Ubuntu', sans-serif;
         font-size: 24px;
         margin-left: 20px;
         margin-right: 30px;
@@ -345,28 +394,31 @@ p{
     text{
         margin-top: 100px;
         color: #38B6FF;
-        font-family: 'Ubuntu', bold;
+        font-family: 'Ubuntu', sans-serif;
         font-weight: 700;
     }
     
     h1{
-        font-size: 20px;
+        font-size: 30px;
         margin-left: 20px;
     }
     
     .OV{
-        font-family: 'Ubuntu', bold;
+        font-family: 'Ubuntu', sans-serif;
         font-size: 15px;
         margin-left: 20px;
     }
     
     p{
-        font-family: 'Ubuntu', regular;
-        font-size: 15px;
-        margin-left: 20px;
+        font-family: 'Ubuntu', sans-serif;
+        font-size: 20px;
+        margin-left: 50px;
         margin-bottom: 100px;
         font-weight: 300;
     }
- 
+
+    h3{
+        font-size: 20px;
+    }
 }
 `
